9f5c7baae50572d678ac677725571416e9975f22
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-08 09:19:28 -0400

    upvote kinda

--- public/JavaScript/site.js
+++ public/JavaScript/site.js
@@ -3,3 +3,10 @@ console.log("TESTING CHECK CHECK");
 
 
 
+function upVoteCheer () {
+
+    let value = parseInt(document.getElementById('numb').value, 10);
+    value = isNaN(value) ? 0 : value; 
+    value ++;
+    document.getElementById('numb').value= value;
+};
