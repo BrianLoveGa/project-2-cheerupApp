de947f4740303bba8595556b47d0f9f5b36f6474
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-08 11:28:42 -0400

    cheerup up vote broke

--- public/JavaScript/site.js
+++ public/JavaScript/site.js
@@ -1,14 +1,29 @@
 console.log("TESTING CHECK CHECK");
 
+// express
+const express = require("express");
+const router = express.Router();
+
+
+// model
+const CheerUps = require("../model/cheerApp");
+
+const cheerController = require('./controller/cheer');
+
+const app = express();
+
+// use controller
+app.use("/cheerups", cheerController);
+
 // cheer up vote count simple style
 
 
 function upVoteCheer () {
 
-    let value = parseInt(document.getElementById('numb').value, 10);
+    let value = parseInt(document.querySelectorAll(this.title).value, 10);
     value = isNaN(value) ? 0 : value; 
     value ++;
-    document.getElementById('numb').value= value;
+    document.getElementById(this.title).value = value;
 };
 
 
