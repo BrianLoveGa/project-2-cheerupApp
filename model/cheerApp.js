47027a981d70e37453cf4720add0716dc178405b
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-07 22:36:27 -0400

    css for phones and what not

--- model/cheerApp.js
+++ model/cheerApp.js
@@ -3,8 +3,11 @@ const mongoose = require("../db/connection");
 const CheerUpsSchema = new mongoose.Schema({
 
     title: String,
+    
     cheerup: String,
+    
     creator: String,
+   
     backgroundColor: Number,
     upvotes: Number
 
