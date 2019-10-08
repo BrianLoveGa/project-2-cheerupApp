58e40412af014a72f9333a1d692d96cebf3c2672
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-08 09:15:50 -0400

    button

--- public/JavaScript/site.js
+++ public/JavaScript/site.js
@@ -1,26 +1,5 @@
 console.log("TESTING CHECK CHECK");
 
-// const cheerUps = document.querySelector('.cheers');
-// const submit = document.getElementById('submit');
 
-// function addCheerUps (cheerData) {
-//     // Adds all of the Cheer Ups to the dom
-//     cheerUps.innerHTML = '';
-//     cheerData.forEach(cheer => {
-//       if (!cheer.title) return
-  
-//       const imageNode = document.createElement('img');
-//       imageNode.setAttribute('src', cheer.title);
-//       imageNode.classList.add('cheerup');
-//       cheerUps.appendChild(imageNode);
-//     });
-//   };
-
-//   /// axios to run get function
-
-// axios.get('http://localhost:3000/cheerups').then(response => {
-//   // gets the initial data
-//   addCheerUps(response.data);
-// });
 
 
